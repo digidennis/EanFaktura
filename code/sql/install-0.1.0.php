5be4f4aa37198ee16cc5392d89ad6693152d5a0a
Dennis Jessen <digidennis3@gmail.com>
2018-02-11 10:53:22 +0000

    init

--- code/sql/install-0.1.0.php
+++ code/sql/install-0.1.0.php
@@ -2,13 +2,4 @@
 
 $installer = $this;
 $installer->startSetup();
-$installer->run("
-ALTER TABLE `{$installer->getTable('sales/quote_payment')}` 
-ADD `ean_nummer` VARCHAR( 255 ) NOT NULL,
-ADD `afstemt` BOOLEAN;
-  
-ALTER TABLE `{$installer->getTable('sales/order_payment')}` 
-ADD `ean_nummer` VARCHAR( 255 ) NOT NULL,
-ADD `afstemt` BOOLEAN;
-");
 $installer->endSetup();
