0eac4bb0f12b78f6b5a16db1122a4645c4bf85c0
Dennis Jessen <digidennis3@gmail.com>
2018-02-19 10:11:32 +0000

    observer

---
+++ code/Model/Observer.php
@@ -0,0 +1,21 @@
+<?php
+
+class Digidennis_EanFaktura_Model_Observer
+{
+
+    public function completeOrderAction($observer)
+    {
+        $block = $observer->getEvent()->getBlock();
+        $order = Mage::registry('current_order');
+
+        if ($order &&
+            $order->getPayment()->getMethodInstance()->getCode()== 'digidennis_eanfaktura' &&
+            $block instanceof Mage_Adminhtml_Block_Sales_Order_View
+        ) {
+            $message = Mage::helper('sales')->__('Are you sure you want to Change Status?');
+            $block->addButton('in_shipment',
+                array( 'label' => Mage::helper('sales')->__($order->getIncrementId()),
+                    'onclick' => "confirmSetLocation('{$message}', '{$block->getUrl('eanfaktura/adminhtml_index/complete')}')", 'class' => 'go' ));
+        }
+    }
+}
